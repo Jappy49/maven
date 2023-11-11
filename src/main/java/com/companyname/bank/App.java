73a0507ce3fd8e74a8e4f489c419a6e451c006ec
Jappy49 <147409830+Jappy49@users.noreply.github.com>
2023-11-11 16:30:55 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "its work" );
+        System.out.println( "itswork" );
         System.out.println("byvee");
       
       
