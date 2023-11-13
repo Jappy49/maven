069cd531b9d2ed32cdb950e918ea888bd10c6334
Jappy49 <147409830+Jappy49@users.noreply.github.com>
2023-11-13 18:29:21 +0530

    a

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "itswork" );
+        System.out.println( "hai" );
         System.out.println("f");
       
       
